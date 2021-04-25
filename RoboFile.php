919f8b5c5223dc83b9155d6a2f7b352c3286a6b3
Mahbubur Rahman <mail@mahbub.me>
2021-04-26 02:52:22 +0600

    Include robo to maintain the build & release process of the plugin

---
+++ RoboFile.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * WP Slideshow Robo Runner Functions
+ *
+ * Declares helper functions to use with Robo.
+ *
+ * @package wp-slideshow
+ * @since 1.0.0
+ */
+
+// Load the autoload file for composer (so we can access RoboCI)
+require_once 'vendor/autoload.php';
+
+/**
+ * WP Slideshow Robo Runner Class
+ *
+ * This system, built on RoboRunner, allows WP Slideshow plugin to be build easily.
+ *
+ * @since 1.0.0
+ * @access public
+ */
+class RoboFile extends \Robo\Tasks {
+
+	public function release( $version ){
+		$this->yell('Releasing WP Slideshow version: ' . $version );
+
+		$this->yell('Minifying JS and CSS');
+		$this->minifyJS();
+		$this->minifyCSS();
+
+		$this->yell('Making POT file');
+		$this->makePotFile();
+
+		$this->yell('Done releasing WP Slideshow version: ' . $version );
+	}
+
+	public function minifyCSS() {
+		// Delete existing minifications
+		foreach ( glob("./assets/css/*.min.css" ) as $filename) {
+			$this->taskExecStack()->stopOnFail()->exec('rm -rf ' . $filename )->run();
+		}
+
+		foreach ( glob("./assets/css/*.css" ) as $filename) {
+			$new_filename = str_replace( '.css', '.min.css', $filename );
+			$this->taskMinify( $filename )->to( $new_filename )->run();
+		}
+	}
+
+	public function minifyJS() {
+		// Delete existing minifications
+		foreach ( glob("./assets/js/*.min.js" ) as $filename) {
+			$this->taskExecStack()->stopOnFail()->exec('rm -rf ' . $filename )->run();
+		}
+
+		foreach ( glob("./assets/js/*.js" ) as $filename) {
+			$new_filename = str_replace( '.js', '.min.js', $filename );
+			$this->taskMinify( $filename )->to( $new_filename )->run();
+		}
+	}
+
+	public function makePotFile() {
+		$this->yell( 'Generating Pot file' );
+		$this->taskExec( './vendor/bin/wp i18n make-pot . languages/wp-slideshow.pot --domain=wp-slideshow --skip-js' )->dir( './' )->run();
+	}
+}
